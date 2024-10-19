0aa12f2c6c244dfead12d5b5b66343dcc1d4caca
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2024-10-19 23:40:04 +0530

    Update AppName.jsx

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,6 +1,6 @@
 import styles from './AppName.module.css';
 function AppName(){
-    return <h1 className={styles.todoHeading}>OrganizeMe</h1>;
+    return <h1 className={styles.todoHeading}>Organize Me</h1>;
 }
 
 export default AppName
